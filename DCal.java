beaf399dc087ae4da07d497ffd879241448a7ae3
YogitaPatil12095 <yogitapatilx12@gmail.com>
2025-07-11 15:51:02 +0530

    Update DCal.java

--- DCal.java
+++ DCal.java
@@ -1,16 +1,21 @@
-import java.util.Scanner;
-public class DCal {
+public class Calculator {
    public static void main(String[] args) {
       double num1;
       double num2;
       double ans;
       char op;
-      Scanner reader = new Scanner(System.in);
-      System.out.print("Enter two numbers: ");
-      num1 = reader.nextDouble();
-      num2 = reader.nextDouble();
-      System.out.print("\nEnter an operator (+, -, *, /): ");
-      op = reader.next().charAt(0);
+
+      // Check if enough arguments are provided
+      if (args.length < 3) {
+         System.out.println("Usage: java Calculator <number1> <number2> <operator>");
+         return;
+      }
+
+      // Parse the arguments
+      num1 = Double.parseDouble(args[0]);
+      num2 = Double.parseDouble(args[1]);
+      op = args[2].charAt(0);
+
       switch(op) {
          case '+': ans = num1 + num2;
             break;
@@ -18,10 +23,16 @@ public class DCal {
             break;
          case '*': ans = num1 * num2;
             break;
-         case '/': ans = num1 / num2;
+         case '/': 
+            if (num2 == 0) {
+               System.out.println("Error: Division by zero is not allowed.");
+               return;
+            }
+            ans = num1 / num2;
             break;
-      default: System.out.printf("Error! Enter correct operator");
-         return;
+         default: 
+            System.out.printf("Error! Enter correct operator");
+            return;
       }
       System.out.print("\nThe result is given as follows:\n");
       System.out.printf(num1 + " " + op + " " + num2 + " = " + ans);
